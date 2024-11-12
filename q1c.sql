95e7bc3032118d22ed69c20a9b22cd276fd1e43e
avVergnet <avergnet@ucsd.edu>
2024-11-12 01:27:28 -0800

    d

--- q1c.sql
+++ q1c.sql
@@ -1,21 +1,37 @@
 -- 1)c) answer to piazza question kinda applies to this
-WITH borrowed AS (
-    SELECT type AS type, COUNT(*) AS occurrence
-    FROM loan l, borrower b
-    WHERE l.no = b.lno
-    GROUP BY l.type
+-- WITH borrowed AS (
+--     SELECT type AS type, COUNT(*) AS occurrence
+--     FROM loan l, borrower b
+--     WHERE l.no = b.lno
+--     GROUP BY l.type
     
-    UNION
+--     UNION
     
-    SELECT type AS type, 0 AS occurrence
-    FROM loan
-    WHERE no NOT IN (SELECT lno FROM borrower)
+--     SELECT type AS type, 0 AS occurrence
+--     FROM loan
+--     WHERE no NOT IN (SELECT lno FROM borrower)
+-- )
+
+-- SELECT b.type
+-- FROM borrowed b
+-- WHERE b.type NOT IN(
+--     SELECT b2.type
+--     FROM borrowed b1, borrowed b2
+--     WHERE b1.occurrence < b2.occurrence
+-- )
+
+
+-- 1)c) answer to piazza question kinda applies to this
+WITH LoanCounts AS (
+  SELECT l.type, COUNT(b.cname) as occurrence
+  FROM loan l LEFT JOIN borrower b ON l.no = b.lno
+  GROUP BY l.type
 )
 
 SELECT b.type
-FROM borrowed b
+FROM LoanCounts b
 WHERE b.type NOT IN(
     SELECT b2.type
-    FROM borrowed b1, borrowed b2
+    FROM LoanCounts b1, LoanCounts b2
     WHERE b1.occurrence < b2.occurrence
 )
